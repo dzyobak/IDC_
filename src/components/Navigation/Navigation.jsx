d7c53158dd00e482d44d38bebf8529ebee94aeaf
dzyobak <ihordzyobak@gmail.com>
2025-01-19 00:46:58 +0200

    Change logo

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,22 +1,30 @@
 import { Link } from "react-router-dom";
 import classes from "../Navigation/Navigation.module.css";
-import Logo from "../../assets/logo3.jpg"
+import Logo from "../../assets/logo3_BW.jpeg";
 
 const Navigation = () => {
   return (
     <nav className={classes.nav}>
       <img className={classes.logo} src={Logo} alt="Logo" />
       <div className={classes.nav_wrapper}>
-        <Link to="/" className={classes.link}>IDC_</Link>
+        <Link to="/" className={classes.link}>
+          IDC_
+        </Link>
       </div>
       <div className={classes.nav_wrapper}>
-        <Link to="/store" className={classes.link}>Store</Link>
+        <Link to="/store" className={classes.link}>
+          Store
+        </Link>
       </div>
       <div className={classes.nav_wrapper}>
-        <Link to="/social" className={classes.link}>Social</Link>
+        <Link to="/social" className={classes.link}>
+          Social
+        </Link>
       </div>
       <div className={classes.nav_wrapper}>
-        <Link to="/vlogs" className={classes.link}>Vlogs</Link>
+        <Link to="/vlogs" className={classes.link}>
+          Vlogs
+        </Link>
       </div>
     </nav>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
-import Nature from "../../assets/photo.jpg"
-import classes from "../Home/Home.module.css"
+import Nature from "../../assets/photo.jpg";
+import classes from "../Home/Home.module.css";
 
 const Home = () => {
-  return <div className={classes.homne_wrapper}>
-    <h1 className={classes.title}>IhorDzobakCandiar</h1>
-    <img src={Nature} alt="Nature" className={classes.photo}/>
-  </div>
-
+  return (
+    <div className={classes.home_wrapper}>
+      <h1 className={classes.title}>IhorDzobakCandiar</h1>
+      <img src={Nature} alt="Nature" className={classes.photo} />
+    </div>
+  );
 };
 
 export default Home;
