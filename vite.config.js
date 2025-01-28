cbd19993022705c4e49bd212726b1da44eb63c08
dzyobak <ihordzyobak@gmail.com>
2025-01-28 15:41:18 +0200

    Додав homepage для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/IDC_/", // Замініть на назву вашого репозиторію
+  base: "/IDC_/", // Додайте назву репозиторію
 });
